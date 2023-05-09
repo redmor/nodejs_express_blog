d5e21a65249f7ef51718dd370d7b20f97cd5a62f
redmor <errabili.oussama@gmail.com>
2023-05-09 17:44:44 -0400

    Day 2

--- server/routes/mainRoute.js
+++ server/routes/mainRoute.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 //Routes
-router.get("", (req, res) => {
-    res.send("Hello world");
-  });
-  
+router.get("/", (req, res) => {
+  res.render("pages/index");
+});
 
-  module.exports = router;
+router.get("/about", (req, res) => {
+  res.render("pages/about");
+});
+
+module.exports = router;
